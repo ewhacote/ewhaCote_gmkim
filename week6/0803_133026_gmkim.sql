cf74549fc55df6523b582642e4a8949b0f6250ba
gkim <kimkm0419@naver.com>
2023-08-03 00:01:55 +0900

    [programmers] 133026 solve

---
+++ week6/0803_133026_gmkim.sql
@@ -0,0 +1,10 @@
+/*
+Programmers 133026 성분으로 구분한 아이스크림 총 주문량
+문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/133026
+*/
+
+SELECT ii.INGREDIENT_TYPE, SUM(fh.TOTAL_ORDER) AS TOTAL_ORDER
+FROM FIRST_HALF fh
+JOIN ICECREAM_INFO ii
+ON fh.FLAVOR = ii.FLAVOR
+GROUP BY ii.INGREDIENT_TYPE
