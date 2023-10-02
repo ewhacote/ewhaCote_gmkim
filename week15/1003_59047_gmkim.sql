e63dc2869dee3c24fbacf591af7c153ba5298831
gkim <kimkm0419@naver.com>
2023-10-03 01:46:28 +0900

    [programmers] 59047 solve

---
+++ week15/1003_59047_gmkim.sql
@@ -0,0 +1,10 @@
+/*
+Programmers 59047 이름에 el이 들어가는 동물 찾기
+문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59047
+*/
+
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+WHERE UPPER(NAME) LIKE "%EL%"
+    AND ANIMAL_TYPE = 'Dog'
+ORDER BY NAME;
