d467c9052224cdfbe485aed5defae65f15a40e57
gkim <kimkm0419@naver.com>
2023-10-04 16:46:06 +0900

    [programmers] 59046 solve

---
+++ week15/1004_59046_gmkim.sql
@@ -0,0 +1,9 @@
+/*
+Programmers 59046 루시와 엘라 찾기
+문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59046
+*/
+
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
+FROM ANIMAL_INS
+WHERE NAME IN ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
+ORDER BY ANIMAL_ID;
