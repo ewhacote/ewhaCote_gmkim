3dfa9f262d5873a152a17d76c71959bfc7f0fbb2
gkim <kimkm0419@naver.com>
2023-10-26 15:40:19 +0900

    [programmers] 42626 solve

---
+++ week18/1026_42626_gmkim.py
@@ -0,0 +1,29 @@
+"""
+Programmers 42626 더 맵게
+문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/42626
+"""
+
+import sys, heapq
+input = sys.stdin.readline
+
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+
+    while scoville[0] < K:
+        if len(scoville) == 1:
+            return -1
+        else:
+            x, y = heapq.heappop(scoville), heapq.heappop(scoville)
+            heapq.heappush(scoville, x + y * 2)
+
+            answer += 1
+
+    return answer
+
+if __name__ == "__main__":
+
+    scoville = list(map(int, input().split()))
+    K = int(input())
+
+    print(solution(scoville, K))
